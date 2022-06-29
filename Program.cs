c38a18f89e3258da5258e642075e4bcc4ce68044
Luiz de Almeida-ADS <luidestec@outlook.com>
2022-06-29 16:02:21 -0300

    Primeiras peças do tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 ﻿using tabuleiro;
 using xadrez_console;
+using xadrez_console.xadrez;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-//Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-//P = new Posicao(3, 4);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-//Console.WriteLine("Posição: " + P);
-Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
-Tela.imprimirTabuleiro(tab);
-
-Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xadrez/Rei.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using tabuleiro;
+using System.Threading.Tasks;
+
+namespace xadrez_console.xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
